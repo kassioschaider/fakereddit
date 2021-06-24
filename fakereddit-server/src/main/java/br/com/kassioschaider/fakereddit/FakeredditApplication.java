582b209b9b24798b5337f64bf4910ceea927c12f
Kassio Schaider <kassio.schaider@hotmail.com>
2021-06-24 15:56:10 -0300

    initial commit

---
+++ fakereddit-server/src/main/java/br/com/kassioschaider/fakereddit/FakeredditApplication.java
@@ -0,0 +1,13 @@
+package br.com.kassioschaider.fakereddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeredditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeredditApplication.class, args);
+	}
+
+}
