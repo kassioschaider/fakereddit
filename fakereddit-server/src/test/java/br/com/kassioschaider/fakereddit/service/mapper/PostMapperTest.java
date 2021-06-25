756c5235229718608514b7b2463cfd1621e9e2a7
Kassio Schaider <kassio.schaider@hotmail.com>
2021-06-25 20:29:00 -0300

    PostMapper tests refactoring

--- fakereddit-server/src/test/java/br/com/kassioschaider/fakereddit/service/mapper/PostMapperTest.java
+++ fakereddit-server/src/test/java/br/com/kassioschaider/fakereddit/service/mapper/PostMapperTest.java
@@ -2,12 +2,14 @@ package br.com.kassioschaider.fakereddit.service.mapper;
 
 import br.com.kassioschaider.fakereddit.model.Post;
 import br.com.kassioschaider.fakereddit.service.dto.PostDTO;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -18,70 +20,80 @@ class PostMapperTest {
     @Autowired
     private PostMapper postMapper;
 
+    private static Post post;
+    private static PostDTO postDTO;
+    private static List<Post> postList;
+    private static List<PostDTO> postDTOList;
+
+    @BeforeAll
+    public static void init() {
+        post = new Post(1L, "CONTENT TEST 1", 1);
+        Post post2 = new Post(2L, "CONTENT TEST 2", 2);
+        Post post3 = new Post(3L, "CONTENT TEST 3", 3);
+
+        postList = Arrays.asList(post, post2, post3);
+
+        postDTO = new PostDTO(1L, "CONTENT TEST 1", 1);
+        PostDTO postDTO2 = new PostDTO(2L, "CONTENT TEST 2", 2);
+        PostDTO postDTO3 = new PostDTO(3L, "CONTENT TEST 3", 3);
+
+        postDTOList = Arrays.asList(postDTO, postDTO2, postDTO3);
+    }
+
     @Test
     public void shouldConvertFromPostToPostDTO() {
-        var post = new Post(1L, "CONTENT TEST", 1);
-        var postDto = postMapper.toDto(post);
+        var newPostDto = postMapper.toDto(post);
 
-        assertEquals(1L, postDto.getId());
-        assertEquals("CONTENT TEST", postDto.getContent());
-        assertEquals(1, postDto.getUpvotes());
+        assertEquals(1L, newPostDto.getId());
+        assertEquals("CONTENT TEST 1", newPostDto.getContent());
+        assertEquals(1, newPostDto.getUpvotes());
     }
 
     @Test
     public void shouldConvertFromPostDTOToPost() {
-        var postDto = new PostDTO(1L, "CONTENT TEST", 1);
-        var post = postMapper.toEntity(postDto);
+        var newPost = postMapper.toEntity(postDTO);
 
-        assertEquals(1L, post.getId());
-        assertEquals("CONTENT TEST", post.getContent());
-        assertEquals(1, post.getUpvotes());
+        assertEquals(1L, newPost.getId());
+        assertEquals("CONTENT TEST 1", newPost.getContent());
+        assertEquals(1, newPost.getUpvotes());
     }
 
     @Test
     public void shouldConvertFromPostArrayToPostDTOArray() {
-        var post = new Post(1L, "CONTENT TEST 1", 1);
-        var post2 = new Post(2L, "CONTENT TEST 2", 2);
-        var post3 = new Post(3L, "CONTENT TEST 3", 3);
-        var postList = Arrays.asList(post, post2, post3);
-        var postDTOList = postMapper.toDto(postList);
+        var newPostDTOList = postMapper.toDto(postList);
 
-        assertEquals(3, postDTOList.size());
+        assertEquals(3, newPostDTOList.size());
 
-        assertEquals(1L, postDTOList.get(0).getId());
-        assertEquals("CONTENT TEST 1", postDTOList.get(0).getContent());
-        assertEquals(1, postDTOList.get(0).getUpvotes());
+        assertEquals(1L, newPostDTOList.get(0).getId());
+        assertEquals("CONTENT TEST 1", newPostDTOList.get(0).getContent());
+        assertEquals(1, newPostDTOList.get(0).getUpvotes());
 
-        assertEquals(2L, postDTOList.get(1).getId());
-        assertEquals("CONTENT TEST 2", postDTOList.get(1).getContent());
-        assertEquals(2, postDTOList.get(1).getUpvotes());
+        assertEquals(2L, newPostDTOList.get(1).getId());
+        assertEquals("CONTENT TEST 2", newPostDTOList.get(1).getContent());
+        assertEquals(2, newPostDTOList.get(1).getUpvotes());
 
-        assertEquals(3L, postDTOList.get(2).getId());
-        assertEquals("CONTENT TEST 3", postDTOList.get(2).getContent());
-        assertEquals(3, postDTOList.get(2).getUpvotes());
+        assertEquals(3L, newPostDTOList.get(2).getId());
+        assertEquals("CONTENT TEST 3", newPostDTOList.get(2).getContent());
+        assertEquals(3, newPostDTOList.get(2).getUpvotes());
     }
 
     @Test
     public void shouldConvertFromPostDTOArrayToPostArray() {
-        var postDto = new PostDTO(1L, "CONTENT TEST 1", 1);
-        var postDto2 = new PostDTO(2L, "CONTENT TEST 2", 2);
-        var postDto3 = new PostDTO(3L, "CONTENT TEST 3", 3);
-        var postDtoList = Arrays.asList(postDto, postDto2, postDto3);
-        var postList = postMapper.toEntity(postDtoList);
+        var newPostList = postMapper.toEntity(postDTOList);
 
-        assertEquals(3, postList.size());
+        assertEquals(3, newPostList.size());
 
-        assertEquals(1L, postList.get(0).getId());
-        assertEquals("CONTENT TEST 1", postList.get(0).getContent());
-        assertEquals(1, postList.get(0).getUpvotes());
+        assertEquals(1L, newPostList.get(0).getId());
+        assertEquals("CONTENT TEST 1", newPostList.get(0).getContent());
+        assertEquals(1, newPostList.get(0).getUpvotes());
 
-        assertEquals(2L, postList.get(1).getId());
-        assertEquals("CONTENT TEST 2", postList.get(1).getContent());
-        assertEquals(2, postList.get(1).getUpvotes());
+        assertEquals(2L, newPostList.get(1).getId());
+        assertEquals("CONTENT TEST 2", newPostList.get(1).getContent());
+        assertEquals(2, newPostList.get(1).getUpvotes());
 
-        assertEquals(3L, postList.get(2).getId());
-        assertEquals("CONTENT TEST 3", postList.get(2).getContent());
-        assertEquals(3, postList.get(2).getUpvotes());
+        assertEquals(3L, newPostList.get(2).getId());
+        assertEquals("CONTENT TEST 3", newPostList.get(2).getContent());
+        assertEquals(3, newPostList.get(2).getUpvotes());
     }
 
 }
