8d2f29da61a8eb751665ab8deb75e5ac4fe0936f
Kassio Schaider <kassio.schaider@hotmail.com>
2021-06-26 17:36:12 -0300

    Initial front

---
+++ fakereddit-client/fakereddit/src/app/new-publish/new-publish.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-new-publish',
+    templateUrl: './new-publish.component.html',
+    styleUrls: ['./new-publish.component.scss']
+})
+export class NewPublishComponent {
+
+}
